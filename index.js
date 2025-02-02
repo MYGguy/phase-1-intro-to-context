908f6aa90b0d05f9d83e129b33150f6e446a5a65
Moshe Goldstein <mygguy613@gmail.com>
2025-02-02 15:25:09 -0500

    add createEmployeeRecords

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
         'payPerHour': payPerHour,
         'timeInEvents': timeInEvents,
         'timeOutEvents': timeOutEvents,
-    }
+    };
 
+    // console.log(employeeObj);
     return employeeObj;
 }
+
+function createEmployeeRecords(arrays) {
+    const employeeRecords = [];
+    arrays.forEach(employee => {
+        // console.log(employee);
+        const record = createEmployeeRecord(employee)
+        employeeRecords.push(record);
+    })
+    // console.log(employeeRecords);
+    return employeeRecords;
+}
+
+createEmployeeRecords([['Bob', 'Wilson', 'Admiral', 35], ['Jon', 'Brannom', 'Captain', 30]])
