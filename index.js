f88e08780cd0c475fc88a6098607f15b1f2a9856
Moshe Goldstein <mygguy613@gmail.com>
2025-02-02 17:47:16 -0500

    add hoursWorkedOnDate func; changed createTime... to .push()

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const testEmployees = [['Bob', 'Wilson', 'Admiral', 35], ['Jon', 'Brannom', 'Captain', 30]];
+const testEmployees = createEmployeeRecords([['Bob', 'Wilson', 'Admiral', 35], ['Jon', 'Brannom', 'Captain', 30]]);
 
 function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
     let timeInEvents = [];
@@ -35,14 +35,11 @@ function createEmployeeRecords(arrays) {
 function createTimeInEvent(employee, dateStamp) {
     const [date, hour] = dateStamp.split(' ');
 
-    // employee.forEach(e => {
-
-    employee.timeInEvents = [{
+    employee.timeInEvents.push({
         'type': 'TimeIn',
         'hour': parseInt(hour, 10),
         'date': date,
-    }]
-    // })
+    });
 
     // console.log(employee);
     return employee;
@@ -52,19 +49,45 @@ function createTimeInEvent(employee, dateStamp) {
 function createTimeOutEvent(employee, dateStamp) {
     const [date, hour] = dateStamp.split(' ');
 
-    // employee.forEach(e => {
-
-    employee.timeOutEvents = [{
+    employee.timeOutEvents.push({
         'type': 'TimeOut',
         'hour': parseInt(hour, 10),
         'date': date,
-    }]
-    // })
+    });
 
     // console.log(employee);
     return employee;
 
 }
+
+// createTimeInEvent(testEmployees[0], '2025-02-02 0900');
+// createTimeOutEvent(testEmployees[0], '2025-02-02 1100');
+// createTimeInEvent(testEmployees[0], '2025-02-03 0900');
+// createTimeOutEvent(testEmployees[0], '2025-02-03 1700');
+
+console.log(testEmployees[0]);
+
+/* REWRITE
+create a function that recieves an employee object and a date.
+    find that date on his object.
+    find the hours clocked in/out on that day.
+    calculate the time difference between the start and end times.
+    return that sum.
+*/
+function hoursWorkedOnDate(employeeObj, dateStamp) {
+    const [getHourIn, getHourOut] = [employeeObj.timeInEvents.find(({ date }) => date === dateStamp).hour, employeeObj.timeOutEvents.find(({ date }) => date === dateStamp).hour];
+
+    const hours = (getHourOut - getHourIn) / 100;
+
+    // console.log(`Clock In: ${getHourIn}. Clock Out: ${getHourOut}`);
+    // console.log(hours);
+
+    return hours;
+
+}
+
+// console.log(hoursWorkedOnDate(testEmployees[0], '2025-02-02'));
+// console.log(hoursWorkedOnDate(testEmployees[0], '2025-02-03'));
 
 // console.log(createTimeInEvent(employeeRecords, '2025-02-02 1500'));
 // console.log(createTimeOutEvent(testEmployees[0], '2025-02-02 2000'));
